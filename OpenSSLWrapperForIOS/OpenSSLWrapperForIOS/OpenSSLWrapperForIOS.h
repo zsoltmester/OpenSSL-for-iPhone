ee971bb799005a5df4457cb18a6328c92f49cc02
Mester, Zsolt <zsolt.mester@misys.com>
2017-06-20 23:36:56 +0200

    Add the public headers to the umbrella header.

--- OpenSSLWrapperForIOS/OpenSSLWrapperForIOS/OpenSSLWrapperForIOS.h
+++ OpenSSLWrapperForIOS/OpenSSLWrapperForIOS/OpenSSLWrapperForIOS.h
@@ -14,6 +14,84 @@ FOUNDATION_EXPORT double OpenSSLWrapperForIOSVersionNumber;
 //! Project version string for OpenSSLWrapperForIOS.
 FOUNDATION_EXPORT const unsigned char OpenSSLWrapperForIOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <OpenSSLWrapperForIOS/PublicHeader.h>
-
-
+// Import all the public headers of this framework.
+#import <OpenSSLWrapperForIOS/aes.h>
+#import <OpenSSLWrapperForIOS/asn1.h>
+#import <OpenSSLWrapperForIOS/asn1_mac.h>
+#import <OpenSSLWrapperForIOS/asn1t.h>
+#import <OpenSSLWrapperForIOS/async.h>
+#import <OpenSSLWrapperForIOS/bio.h>
+#import <OpenSSLWrapperForIOS/blowfish.h>
+#import <OpenSSLWrapperForIOS/bn.h>
+#import <OpenSSLWrapperForIOS/buffer.h>
+#import <OpenSSLWrapperForIOS/camellia.h>
+#import <OpenSSLWrapperForIOS/cast.h>
+#import <OpenSSLWrapperForIOS/cmac.h>
+#import <OpenSSLWrapperForIOS/cms.h>
+#import <OpenSSLWrapperForIOS/comp.h>
+#import <OpenSSLWrapperForIOS/conf.h>
+#import <OpenSSLWrapperForIOS/conf_api.h>
+#import <OpenSSLWrapperForIOS/crypto.h>
+#import <OpenSSLWrapperForIOS/ct.h>
+#import <OpenSSLWrapperForIOS/des.h>
+#import <OpenSSLWrapperForIOS/dh.h>
+#import <OpenSSLWrapperForIOS/dsa.h>
+#import <OpenSSLWrapperForIOS/dtls1.h>
+#import <OpenSSLWrapperForIOS/e_os2.h>
+#import <OpenSSLWrapperForIOS/ebcdic.h>
+#import <OpenSSLWrapperForIOS/ec.h>
+#import <OpenSSLWrapperForIOS/ecdh.h>
+#import <OpenSSLWrapperForIOS/ecdsa.h>
+#import <OpenSSLWrapperForIOS/engine.h>
+#import <OpenSSLWrapperForIOS/err.h>
+#import <OpenSSLWrapperForIOS/evp.h>
+#import <OpenSSLWrapperForIOS/hmac.h>
+#import <OpenSSLWrapperForIOS/idea.h>
+#import <OpenSSLWrapperForIOS/kdf.h>
+#import <OpenSSLWrapperForIOS/lhash.h>
+#import <OpenSSLWrapperForIOS/md2.h>
+#import <OpenSSLWrapperForIOS/md4.h>
+#import <OpenSSLWrapperForIOS/md5.h>
+#import <OpenSSLWrapperForIOS/mdc2.h>
+#import <OpenSSLWrapperForIOS/modes.h>
+#import <OpenSSLWrapperForIOS/obj_mac.h>
+#import <OpenSSLWrapperForIOS/objects.h>
+#import <OpenSSLWrapperForIOS/ocsp.h>
+#import <OpenSSLWrapperForIOS/opensslconf.h>
+#import <OpenSSLWrapperForIOS/opensslconf_ios_arm64.h>
+#import <OpenSSLWrapperForIOS/opensslconf_ios_armv7.h>
+#import <OpenSSLWrapperForIOS/opensslconf_ios_armv7s.h>
+#import <OpenSSLWrapperForIOS/opensslconf_ios_i386.h>
+#import <OpenSSLWrapperForIOS/opensslconf_ios_x86_64.h>
+#import <OpenSSLWrapperForIOS/opensslconf_tvos_arm64.h>
+#import <OpenSSLWrapperForIOS/opensslconf_tvos_x86_64.h>
+#import <OpenSSLWrapperForIOS/opensslv.h>
+#import <OpenSSLWrapperForIOS/ossl_typ.h>
+#import <OpenSSLWrapperForIOS/pem.h>
+#import <OpenSSLWrapperForIOS/pem2.h>
+#import <OpenSSLWrapperForIOS/pkcs12.h>
+#import <OpenSSLWrapperForIOS/pkcs7.h>
+#import <OpenSSLWrapperForIOS/rand.h>
+#import <OpenSSLWrapperForIOS/rc2.h>
+#import <OpenSSLWrapperForIOS/rc4.h>
+#import <OpenSSLWrapperForIOS/rc5.h>
+#import <OpenSSLWrapperForIOS/ripemd.h>
+#import <OpenSSLWrapperForIOS/rsa.h>
+#import <OpenSSLWrapperForIOS/safestack.h>
+#import <OpenSSLWrapperForIOS/seed.h>
+#import <OpenSSLWrapperForIOS/sha.h>
+#import <OpenSSLWrapperForIOS/srp.h>
+#import <OpenSSLWrapperForIOS/srtp.h>
+#import <OpenSSLWrapperForIOS/ssl.h>
+#import <OpenSSLWrapperForIOS/ssl2.h>
+#import <OpenSSLWrapperForIOS/ssl3.h>
+#import <OpenSSLWrapperForIOS/stack.h>
+#import <OpenSSLWrapperForIOS/symhacks.h>
+#import <OpenSSLWrapperForIOS/tls1.h>
+#import <OpenSSLWrapperForIOS/ts.h>
+#import <OpenSSLWrapperForIOS/txt_db.h>
+#import <OpenSSLWrapperForIOS/ui.h>
+#import <OpenSSLWrapperForIOS/whrlpool.h>
+#import <OpenSSLWrapperForIOS/x509.h>
+#import <OpenSSLWrapperForIOS/x509_vfy.h>
+#import <OpenSSLWrapperForIOS/x509v3.h>
